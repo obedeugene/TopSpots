9e46431c306c6dc1a637d532dce48388bc1aa102
Obed Eugene <obed.eugene@gmail.com>
2014-04-13 19:39:58 -0400

    Added some tests using rspec and capybara

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Pages" do
+  describe "GET /pages" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get root_path
+      response.status.should be(200)
+    end
+  end
+
+  describe "Index Page" do
+  	it "Should have Post Title" do
+  	   visit 'posts_path' 
+  	   page.should have_content(post: title)
+    end
+  end
+end
